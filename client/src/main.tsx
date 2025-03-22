5f45b3adf40821fee7fb0a9d37cff5aa1d18ff06
Builder106 <vaughanolayinka@gmail.com>
2025-03-22 08:01:23 +0100

    Refactor CSS structure: remove old index.css, add index.tailwind.css for improved Tailwind CSS integration and theme management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.tailwind.css";
 import { ThemeProvider } from './contexts/ThemeContext';
 
 // Add global styles
